0a7b55f9bd93da92b49606c1b0c17ec715180cd3
P Nobel <pnob99@gmail.com>
2017-05-05 20:53:03 +0000

    Character.java created online with Bitbucket

---
+++ Character.java
@@ -0,0 +1,39 @@
+
+/**
+ * Characters is the base class of all defenders and actors
+ * 
+ * @author Parth
+ * @version 0.0
+ */
+abstract public class Character
+{
+    Location loc;
+    private int hp;
+    public Character(int baseHP, Location loc)
+    {
+        hp = baseHP;
+        this.loc = loc;
+    }
+
+    public Location getLoc()
+    {
+        return loc;
+    }
+
+    public void setLoc(Location loc)
+    {
+        this.loc = loc;
+    }
+
+    public boolean giveDamage(int damage)
+    {
+        hp -= damage;
+        return hp > 0;
+    }
+
+    public boolean dead()
+    {
+        return hp <= 0;
+    }
+
+}
