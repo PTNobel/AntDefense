0297378546e126ed5da84d95ea1ef5de91b7e1c7
Ignatius Widjaja <huang.ij5@gmail.com>
2017-05-16 20:54:00 +0000

    DancingAnt.java created online with Bitbucket

---
+++ DancingAnt.java
@@ -0,0 +1,42 @@
+import java.util.List;
+import java.util.LinkedList;
+import java.util.ListIterator;
+/**
+ * Write a description of class DancingAnt here.
+ * 
+ * @author Ignatius Widjaja
+ * @version 0.0
+ */
+public class DancingAnt extends Ant
+{
+    private int swing;
+    
+    public DancingAnt(int baseHP, Location loc)
+    {
+        super(baseHP, loc);
+        swing = 12;
+    }
+
+    public List<Defender> act(List<Defender> defenders)
+    {
+        List<Defender> d = new LinkedList<Defender>();
+        for (Defender defense : defenders)
+        {
+            if (loc.getCol() == defense.getLoc().getCol() && loc.getRow() == defense.getLoc().getCol())
+            {
+                if(!defense.takeDamage(swing))
+                    d.add(defense);
+            }
+            else
+                move(loc);
+
+        }
+        return d;
+    }
+
+    private void move(Location loc)
+    {
+        int thing = (int)(Math.random()*8)*45;
+        
+    }
+}
