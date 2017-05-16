7c94e134a9b788eb8ec001a36431d9004a348ad1
Parth Nobel <pnob99@gmail.com>
2017-05-16 14:24:24 -0700

    Implemnts and comments LevelGenerator

--- LevelGenerator.java
+++ LevelGenerator.java
@@ -1,3 +1,15 @@
+import java.util.List;
+
 public interface LevelGenerator
 {
+    // The total number of attackers in this round.
+    public int getNumAttackers();
+
+    // Each turn this will be called. It will return a list of ants for that
+    // turn.
+    // i.e. on turn 1 it may return no ants.
+    // On turn 3 it may return a single worker ant
+    // On turn 50 it may return a queen ant and three warrior ants
+    // These ants will recieve their location from level generator.
+    public List<Ant> generateAnts();
 }
