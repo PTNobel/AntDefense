36ad170f943cd8bbe7c8bfadb9515cb14e426654
Ignatius Widjaja <huang.ij5@gmail.com>
2017-05-16 20:54:46 +0000

    QueenAnt.java created online with Bitbucket

---
+++ QueenAnt.java
@@ -0,0 +1,40 @@
+import java.util.List;
+import java.util.LinkedList;
+import java.util.ListIterator;
+/**
+ * Write a description of class QueenAnt here.
+ * 
+ * @author Ignatius Widjaja
+ * @version 0.0
+ */
+public class QueenAnt extends Ant
+{
+    private int swing;
+    public QueenAnt(int baseHP, Location loc)
+    {
+        super(baseHP, loc);
+        swing = 25;
+    }
+
+    public List<Defender> act(List<Defender> defenders)
+    {
+        List<Defender> d = new LinkedList<Defender>();
+        for (Defender defense : defenders)
+        {
+            if (loc.getCol() == defense.getLoc().getCol() && loc.getRow() == defense.getLoc().getCol())
+            {
+                if(!defense.takeDamage(swing))
+                    d.add(defense);
+            }
+            else
+                move(loc);
+
+        }
+        return d;
+    }
+
+    private void move(Location loc)
+    {
+        setLoc(new Location(loc.getX() - 10, loc.getY()));
+    }
+}
