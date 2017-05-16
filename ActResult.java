66061e8d7364699e846b6aca0916466065edc4d3
Chase Carnaroli <chasecarnaroli@gmail.com>
2017-05-16 21:24:52 +0000

    added get/set methods

--- ActResult.java
+++ ActResult.java
@@ -7,4 +7,48 @@ public class ActResult
     public boolean cakeEaten;
     public int progress;
     List<Character> movedCharacters;
+    
+    public ActResult(){
+        // do we need anything in here?
+    }
+    
+    public void setDeadCharacters(List<Character> deadCharacters){
+        this.deadCharacters = deadCharacters;
+    }
+    
+    public List<Character> getDeadCharacters(){
+        return deadCharacters;
+    }
+    
+    public void setNewCharacters(List<Character> newCharacters){
+        this.newCharacters = newCharacters;
+    }
+    
+    public List<Character> getNewCharacters(){
+        return newCharacters;
+    }
+    
+    public void setMovedCharacters(List<Character> movedCharacters){
+        this.movedCharacters = movedCharacters;
+    }
+    
+    public List<Character> setMovedCharacters(){
+        return movedCharacters;
+    }
+    
+    public void setCakeEater(boolean eaten){
+        cakeEaten = eaten;
+    }
+    
+    public boolean getCakeEaten(){
+        return cakeEaten;
+    }
+    
+    public void progressIncreased(){
+        // finish later
+    }
+    
+    public void getProgress(){
+        // finish later
+    }
 }
