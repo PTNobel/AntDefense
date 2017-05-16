13cbb2d3af1ca433931df47621c0f0d7247871b0
Parth Nobel <pnob99@gmail.com>
2017-05-16 13:31:01 -0700

    Adds ActResult.

---
+++ ActResult.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class ActResult
+{
+    public List<Character> deadCharacters;
+    public List<Character> newCharacters;
+    public boolean cakeEaten;
+    public int progress;
+    List<Character> movedCharacters;
+}
