7a441e111f0fdcc413c24eb9f5603e9363723801
Chase Carnaroli <chasecarnaroli@gmail.com>
2017-05-30 21:02:05 +0000

    changed from interface to abstract super

--- LevelGenerator.java
+++ LevelGenerator.java
@@ -1,9 +1,18 @@
 import java.util.List;
 
-public interface LevelGenerator
+public abstract class LevelGenerator
 {
+    protected int completed = 0; 
+    protected int max;
+    
+    public LevelGenerator(int max){
+        this.max = max;
+    }
+    
     // The total number of attackers in this round.
-    public int getNumAttackers();
+    public int getNumAttackers(){
+        return max;
+    }
 
     // Each turn this will be called. It will return a list of ants for that
     // turn.
@@ -11,5 +20,5 @@ public interface LevelGenerator
     // On turn 3 it may return a single worker ant
     // On turn 50 it may return a queen ant and three warrior ants
     // These ants will recieve their location from level generator.
-    public List<Ant> generateAnts();
+    public abstract List<Ant> generateAnts();
 }
