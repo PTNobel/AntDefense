4e8b63d57d0d3d4e572121bbc6e62c5326e78ccb
Chase Carnaroli <chasecarnaroli@gmail.com>
2017-05-30 21:23:51 +0000

    added row locations and selectRandomRow()

--- LevelGenerator.java
+++ LevelGenerator.java
@@ -2,8 +2,25 @@ import java.util.List;
 
 public abstract class LevelGenerator
 {
+    // instance variables
     protected int completed = 0; 
     protected int max;
+    
+    // location variables
+    private int numRows = 5
+    private int yGap = 40
+    private int xStart = 760;
+    protected List<Location> rows = new ArrayList<Location>();
+    protected Location row1 = new Location(xStart, yGap);
+    protected Location row2 = new Location(xStart, yGap * 2);
+    protected Location row3 = new Location(xStart, yGap * 3);
+    protected Location row4 = new Location(xStart, yGap * 4);
+    protected Location row5 = new Location(xStart, yGap * 5);
+    rows.add(row1);
+    rows.add(row2);
+    rows.add(row3);
+    rows.add(row4);
+    rows.add(row5);
     
     public LevelGenerator(int max){
         this.max = max;
@@ -12,6 +29,11 @@ public abstract class LevelGenerator
     // The total number of attackers in this round.
     public int getNumAttackers(){
         return max;
+    }
+
+    public Location selectRandomRow(){
+        int randomRow = (int)(Math.random()*numRows);
+        return rows.get(randomRow);
     }
 
     // Each turn this will be called. It will return a list of ants for that
