ee69a5eba585bfa20c53af92efe4dd863e19b943
Tiffany Nguyen <thao13039@gmail.com>
2017-05-31 21:20:40 +0000

    switched to PictureLoader

--- ReidAnt.java
+++ ReidAnt.java
@@ -49,6 +49,6 @@ public class ReidAnt extends Ant
 	    return 10;
     }
     public ImageIcon getInitialImageIcon(){
-        return new ImageIcon("reidAnt.png");
+        return PictureLoader.reidAnt;
     }
 }
