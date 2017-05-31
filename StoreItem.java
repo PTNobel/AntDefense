eced7f64f36e3fd4af85e42289457c229bf099fd
Parth Nobel <pnob99@gmail.com>
2017-05-30 20:29:37 -0700

    Adds new StoreItems

--- StoreItem.java
+++ StoreItem.java
@@ -3,7 +3,9 @@ import javax.swing.ImageIcon;
 public enum StoreItem
 {
     POISONFOOD (25, new ImageIcon("poisonFood.png")),
-    POISONSPRAY (50, new ImageIcon("poisonSpray.png"));
+    WOODENBLOCK (25, new ImageIcon("woodenBlock.png")),
+    POISONSPRAY (50, new ImageIcon("poisonSpray.png")),
+    SQUIRTGUN (100, new ImageIcon("squirtGun.png"));
 
     public final int COST;
     public final ImageIcon INIT_IMAGE;
@@ -24,6 +26,10 @@ public enum StoreItem
                 return new PoisonFood(loc);
             case POISONSPRAY:
                 return new PoisonSpray(loc);
+            case WOODENBLOCK:
+                return new WoodenBlock(loc);
+            case SQUIRTGUN:
+                return new SquirtGun(loc);
             default:
                 return null;
         }
