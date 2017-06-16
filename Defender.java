d6103e1ed8f774f0b2493baea343150f3d346ce6
Parth Nobel <pnob99@gmail.com>
2017-06-16 12:30:53 -0700

    Adds license notice to all java files

--- Defender.java
+++ Defender.java
@@ -1,3 +1,24 @@
+/*
+ *
+ * Copyright 2017 Parth Nobel, Chase Carnaroli, Tiffany Nguyen, and Ignatius Widjaja
+ *
+ * This file is part of Ant Defense.
+ *
+ * Ant Defense is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Ant Defense is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Ant Defense.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 import java.util.List;
 import javax.swing.ImageIcon;
 
