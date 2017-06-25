35837d0653237a9bd6620fb058b67aab8808fa97
Parth Nobel <pnob99@gmail.com>
2017-06-25 15:35:56 -0700

    Fixes the Mac bug by manually forcing look and feel in AntDefense and TrivialDefense

--- AntDefense.java
+++ AntDefense.java
@@ -20,11 +20,32 @@
  */
 
 import javax.swing.JOptionPane; 
+import javax.swing.UIManager; 
+import javax.swing.UnsupportedLookAndFeelException; 
 
 public class AntDefense
 {
     public static void main(String[] args)
     {
+        
+        try {
+            // Set cross-platform Java L&F (also called "Metal")
+            UIManager.setLookAndFeel(
+                UIManager.getCrossPlatformLookAndFeelClassName());
+        } 
+        catch (UnsupportedLookAndFeelException e) {
+            // handle exception
+        }
+        catch (ClassNotFoundException e) {
+            // handle exception
+        }
+        catch (InstantiationException e) {
+            // handle exception
+        }
+        catch (IllegalAccessException e) {
+            // handle exception
+        }
+
         PictureLoader picLoader =  new PictureLoader();
 
         Model game = new Model(getDifficulty());
--- TrivialDefense.java
+++ TrivialDefense.java
@@ -20,11 +20,32 @@
  */
 
 import javax.swing.JOptionPane; 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class TrivialDefense
 {
     public static void main(String[] args)
     {
+        try {
+            // Set cross-platform Java L&F (also called "Metal")
+            UIManager.setLookAndFeel(
+                UIManager.getCrossPlatformLookAndFeelClassName());
+        } 
+        catch (UnsupportedLookAndFeelException e) {
+            // handle exception
+        }
+        catch (ClassNotFoundException e) {
+            // handle exception
+        }
+        catch (InstantiationException e) {
+            // handle exception
+        }
+        catch (IllegalAccessException e) {
+            // handle exception
+        }
+        
+
         PictureLoader picLoader =  new PictureLoader();
         LevelGenerator lg = new TrivialGenerator();
 
